b9f94b26aa7154c7f315147f3670d3b1e0c34d5c
Gabreu20 <48576715+Gabreu20@users.noreply.github.com>
2022-05-26 15:30:07 -0300

    Add files via upload

---
+++ src/main/java/gabriel_lucas/atividade3/classes/Aluno.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gabriel_lucas.atividade3.classes;
+
+/**
+ *
+ * @author gabriel
+ */
+public class Aluno extends Usuario{
+    public String matricula;
+    public String curso; 
+    public int copiasGratuitas = 50;
+    public int copiasFeitas = 0;
+    
+    public Aluno(String n, int i, String s, String dN,String m, String c){
+        this.nome = n;
+        this.idade = i;
+        this.sexo = s;
+        this.dataNascimento = dN;
+        
+        matricula = m;
+        curso = c;
+    }
+}
+
---
+++ src/main/java/gabriel_lucas/atividade3/classes/Secretario.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gabriel_lucas.atividade3.classes;
+
+/**
+ *
+ * @author gabriel
+ */
+public class Secretario extends Funcionario {
+    String horarioTrabalho;
+}
